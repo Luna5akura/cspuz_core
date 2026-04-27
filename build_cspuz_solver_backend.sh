9fcbf73101ecc381c7d09df31a6166cd9fb977c9
luna5akura <734514717@qq.com>
2026-04-27 18:11:40 +0800

    update

--- build_cspuz_solver_backend.sh
+++ build_cspuz_solver_backend.sh
@@ -15,4 +15,12 @@ fi
 
 mkdir -p build/cspuz_solver_backend
 cp target/wasm32-unknown-emscripten/${MODE}/deps/cspuz_solver_backend.js build/cspuz_solver_backend/
-cp target/wasm32-unknown-emscripten/${MODE}/deps/cspuz_solver_backend.wasm build/cspuz_solver_backend/
+if [ -f target/wasm32-unknown-emscripten/${MODE}/deps/cspuz_solver_backend.wasm ]; then
+    cp target/wasm32-unknown-emscripten/${MODE}/deps/cspuz_solver_backend.wasm build/cspuz_solver_backend/
+elif [ -f target/wasm32-unknown-emscripten/${MODE}/cspuz_solver_backend.wasm ]; then
+    cp target/wasm32-unknown-emscripten/${MODE}/cspuz_solver_backend.wasm build/cspuz_solver_backend/
+else
+    echo "cspuz_solver_backend.wasm was not found after build"
+    find target/wasm32-unknown-emscripten/${MODE} -maxdepth 2 -type f | sort
+    exit 1
+fi
